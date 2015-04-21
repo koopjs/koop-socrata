4f9b54bb3d7e5b0766524170ac18e34017a2670e
chelm <christopher.helm@gmail.com>
2015-04-21 10:01:46 -0600

    creating v0.1.0; using standard format and better test example

--- models/Socrata.js
+++ models/Socrata.js
@@ -1,187 +1,199 @@
-var request = require('request');
+var request = require('request')
 
-var Socrata = function( koop ){
+var Socrata = function (koop) {
+  var socrata = koop.BaseModel(koop)
 
-  var socrata = {};
-  socrata.__proto__ = koop.BaseModel( koop );
+  // wrap request into a central place
+  socrata.request = function (url, callback) {
+    request.get(url, callback)
+  }
 
   // adds a service to the koop.Cache.db
   // needs a host, generates an id
-  socrata.register = function( id, host, callback ){
-    var type = 'socrata:services';
-    koop.Cache.db.serviceCount( type, function(error, count){
-      id = id || count++;
-      koop.Cache.db.serviceRegister( type, {'id': id, 'host': host},  function( err, success ){
-        callback( err, id );
-      });
-    });
-  };
+  socrata.register = function (id, host, callback) {
+    var type = 'socrata:services'
+    koop.Cache.db.serviceCount(type, function (error, count) {
+      if (error) {
+        return callback(error, null)
+      }
+      id = id || count++
+      koop.Cache.db.serviceRegister(type, {'id': id, 'host': host}, function (err, success) {
+        callback(err, id)
+      })
+    })
+  }
 
-  socrata.remove = function( id, callback ){
-    koop.Cache.db.serviceRemove( 'socrata:services', parseInt(id) || id,  callback);
-  };
+  socrata.remove = function (id, callback) {
+    koop.Cache.db.serviceRemove('socrata:services', parseInt(id, 0) || id, callback)
+  }
 
   // get service by id, no id == return all
-  socrata.find = function( id, callback ){
-    koop.Cache.db.serviceGet( 'socrata:services', parseInt(id) || id, function(err, res){
-      if (err){
-        callback('No datastores have been registered with this provider yet. Try POSTing {"host":"url", "id":"yourId"} to /socrata', null);
+  socrata.find = function (id, callback) {
+    koop.Cache.db.serviceGet('socrata:services', parseInt(id, 0) || id, function (err, res) {
+      if (err) {
+        callback('No datastores have been registered with this provider yet. Try POSTing {"host":"url", "id":"yourId"} to /socrata', null)
+      } else {
+        callback(null, res)
       }
-      else{
-        callback(null, res);
-      }
-    });
-  };
+    })
+  }
 
-  socrata.socrata_path = '/resource/';
-  socrata.socrata_view_path = '/resource/';
+  socrata.socrata_path = '/resource/'
+  socrata.socrata_view_path = '/resource/'
 
   // got the service and get the item
-  socrata.getResource = function( host, hostId, id, options, callback ){
-    var self = this,
-      type = 'Socrata',
-      key = id;
+  socrata.getResource = function (host, hostId, id, options, callback) {
+    var fields, types,
+    type = 'Socrata',
+    key = id
 
-    koop.Cache.get( type, key, options, function(err, entry ){
-      if ( err ){
-        var url = host + self.socrata_path + id + '.json';
-        var meta_url = host + self.socrata_view_path + id + '.json';
-        //dmf: have to make a request to the views endpoint in order to get metadata
-        var name;
-        request.get(meta_url, function(err, data, response){
-          if (err){
-            callback(err, null);
+    koop.Cache.get(type, key, options, function (err, entry) {
+      if (err) {
+        var url = host + socrata.socrata_path + id + '.json'
+        var meta_url = host + socrata.socrata_view_path + id + '.json'
+        // dmf: have to make a request to the views endpoint in order to get metadata
+        var name
+
+        socrata.request(meta_url, function (err, data, response) {
+          if (err) {
+            callback(err, null)
           } else {
             try {
-              name = JSON.parse( data.body ).name;
-            } catch( e ){
-              callback(e, null);
+              name = JSON.parse(data.body).name
+            } catch(e) {
+              callback(e, null)
             }
           }
-
-          request.get(url, function(err, data, response ){
+          socrata.request(url, function (err, data, response) {
             if (err) {
-              callback(err, null);
+              callback(err, null)
             } else {
               try {
-                var types = JSON.parse( data.headers['x-soda2-types'] );
-                  fields = JSON.parse( data.headers['x-soda2-fields'] );
-                var locationField;
-                types.forEach(function(t,i){
-                  if (t == 'location'){
-                    locationField = fields[i];
+                types = JSON.parse(data.headers['x-soda2-types'])
+                fields = JSON.parse(data.headers['x-soda2-fields'])
+                var locationField
+                types.forEach(function (t, i) {
+                  if (t === 'location') {
+                    locationField = fields[i]
                   }
-                });
- 
-                self.toGeojson( JSON.parse( data.body ), locationField, function(err, geojson){
-                  geojson.updated_at = new Date(data.headers['last-modified']).getTime();
-                  geojson.name = id;
+                })
+
+                socrata.toGeojson(JSON.parse(data.body), locationField, function (err, geojson) {
+                  if (err) {
+                    return callback(err)
+                  }
+                  geojson.updated_at = new Date(data.headers['last-modified']).getTime()
+                  geojson.name = name || id
                   geojson.host = {
                     id: hostId,
                     url: host
-                  };
-                  koop.Cache.insert( type, key, geojson, 0, function( err, success){
-                    if ( success ) callback( null, [geojson] );
-                  });
-                });
-              } catch(e){
-                console.log('shit?', e);
-                koop.log.error('Unable to parse response %s', url);
-                callback(e, null); 
+                  }
+                  koop.Cache.insert(type, key, geojson, 0, function (err, success) {
+                    if (err) {
+                      return callback(err)
+                    }
+                    callback(null, [geojson])
+                  })
+                })
+              } catch(e) {
+                if (koop && koop.log) {
+                  koop.log.error('Unable to parse response %s', url)
+                }
+                callback(e, null)
               }
             }
-          });
-        });
+          })
+        })
       } else {
-        callback( null, entry );
+        callback(null, entry)
       }
-    });
+    })
+  }
 
-  };
-
-  socrata.toGeojson = function(json, locationField, callback){
-    if (!json || !json.length){
-      callback('Error converting data to geojson', null);
+  socrata.toGeojson = function (json, locationField, callback) {
+    if (!json || !json.length) {
+      callback('Error converting data to geojson', null)
     } else {
-      var geojson = {type: 'FeatureCollection', features: []};
-      var geojsonFeature;
-      json.forEach(function(feature, i){
-        geojsonFeature = {type: 'Feature', geometry: {}, id: i+1};
-        if (feature && locationField){
-          if (feature[locationField] && feature[locationField].latitude && feature[locationField].longitude){
-            geojsonFeature.geometry.coordinates = [parseFloat(feature[locationField].longitude), parseFloat(feature[locationField].latitude)];
-            geojsonFeature.geometry.type = 'Point';
-            delete feature.location;
-            geojsonFeature.properties = feature;
-            geojson.features.push( geojsonFeature );
+      var geojson = {type: 'FeatureCollection', features: []}
+      var geojsonFeature
+      json.forEach(function (feature, i) {
+        geojsonFeature = {type: 'Feature', geometry: {}, id: i + 1}
+        if (feature && locationField) {
+          if (feature[locationField] && feature[locationField].latitude && feature[locationField].longitude) {
+            geojsonFeature.geometry.coordinates = [parseFloat(feature[locationField].longitude), parseFloat(feature[locationField].latitude)]
+            geojsonFeature.geometry.type = 'Point'
+            delete feature.location
+            geojsonFeature.properties = feature
+            geojson.features.push(geojsonFeature)
           }
-        } else if ( feature && feature.latitude && feature.longitude ){
-           geojsonFeature.geometry.coordinates = [parseFloat(feature.longitude), parseFloat(feature.latitude)];
-           geojsonFeature.geometry.type = 'Point';
-           geojsonFeature.properties = feature;
-           geojson.features.push( geojsonFeature );
+        } else if (feature && feature.latitude && feature.longitude) {
+          geojsonFeature.geometry.coordinates = [parseFloat(feature.longitude), parseFloat(feature.latitude)]
+          geojsonFeature.geometry.type = 'Point'
+          geojsonFeature.properties = feature
+          geojson.features.push(geojsonFeature)
         } else {
-          geojsonFeature.geometry = null;
-          geojsonFeature.properties = feature;
-          geojson.features.push( geojsonFeature );
+          geojsonFeature.geometry = null
+          geojsonFeature.properties = feature
+          geojson.features.push(geojsonFeature)
         }
-      });
-      callback(null, geojson);
+      })
+      callback(null, geojson)
     }
-  };
+  }
 
   // compares the sha on the cached data and the hosted data
   // this method name is special reserved name that will get called by the cache model
-  socrata.checkCache = function(key, data, options, callback){
-    var self = this;
-    url = data.host + this.socrata_path + key + '.json';
-
-    var lapsed = (new Date().getTime() - data.updated_at);
-    if (typeof(data.updated_at) == "undefined" || (lapsed > (1000*60*60))){
-      callback(null, false);
+  socrata.checkCache = function (key, data, options, callback) {
+    var url = data.host + this.socrata_path + key + '.json'
+    var lapsed = (new Date().getTime() - data.updated_at)
+    if (typeof (data.updated_at) === 'undefined' || (lapsed > (1000 * 60 * 60))) {
+      callback(null, false)
     } else {
-      request.get(url, function( err, data, response ){
+      socrata.request(url, function (err, data, response) {
         if (err) {
-          callback( err, null );
+          callback(err, null)
         } else {
-          var types = JSON.parse( data.headers['x-soda2-types'] );
-          var fields = JSON.parse( data.headers['x-soda2-fields'] );
-          var locationField;
-          types.forEach(function(t,i){
-            if (t == 'location'){
-              locationField = fields[i];
+          var types = JSON.parse(data.headers['x-soda2-types'])
+          var fields = JSON.parse(data.headers['x-soda2-fields'])
+          var locationField
+          types.forEach(function (t, i) {
+            if (t === 'location') {
+              locationField = fields[i]
             }
-          });
-          self.toGeojson( JSON.parse( data.body ), locationField, function( error, geojson ){
-            geojson.updated_at = new Date(data.headers['last-modified']).getTime();
-            geojson.name = data.name || key;
-            geojson.host = data.host;
-            callback( error, [geojson] );
-          });
+          })
+          socrata.toGeojson(JSON.parse(data.body), locationField, function (error, geojson) {
+            geojson.updated_at = new Date(data.headers['last-modified']).getTime()
+            geojson.name = data.name || key
+            geojson.host = data.host
+            callback(error, [geojson])
+          })
         }
-      });
+      })
     }
+  }
 
-  };
+  // drops the item from the cache
+  socrata.dropItem = function (host, itemId, options, callback) {
+    var dir = [ 'socrata', host, itemId].join(':'),
+    errors = []
 
-   // drops the item from the cache
-  socrata.dropItem = function( host, itemId, options, callback ){
-    var dir = [ 'socrata', host, itemId].join(':');
-    koop.Cache.remove('Socrata', itemId, options, function(err, res){
-      koop.files.removeDir( 'files/' + dir, function(err, res){
-        koop.files.removeDir( 'tiles/'+ dir, function(err, res){
-          koop.files.removeDir( 'thumbs/'+ dir, function(err, res){
-            callback(err, true);
-          });
-        });
-      });
-    });
-  };
+    koop.Cache.remove('Socrata', itemId, options, function (err, res) {
+      if (err) errors.push(err)
+      koop.files.removeDir('files/' + dir, function (err, res) {
+        if (err) errors.push(err)
+        koop.files.removeDir('tiles/' + dir, function (err, res) {
+          if (err) errors.push(err)
+          koop.files.removeDir('thumbs/' + dir, function (err, res) {
+            if (err) errors.push(err)
+            callback(errors.join(', '), true)
+          })
+        })
+      })
+    })
+  }
 
-  return socrata;
+  return socrata
 
-};
+}
 
-
-module.exports = Socrata;
-
+module.exports = Socrata
