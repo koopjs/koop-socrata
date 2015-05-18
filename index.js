d63df5677a54ee35be67e32b84c6a619cfe55874
chelm <christopher.helm@gmail.com>
2015-05-18 15:17:56 -0600

    no more semicolons

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var pjson = require('./package.json');
+var pjson = require('./package.json')
 
 exports.name = 'Socrata'
 exports.hosts = true
 exports.controller = require('./controller')
 exports.routes = require('./routes')
 exports.model = require('./models/Socrata.js')
-exports.status = { version: pjson.version};
+exports.status = { version: pjson.version}
